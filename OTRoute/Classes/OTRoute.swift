7249e82f54816f7bba92624500da676038220a60
hanlei <hanlei@meiqia.com>
2018-04-12 11:50:51 +0800

    remove: OTRoute+ClassType

---
+++ OTRoute/Classes/OTRoute.swift
@@ -0,0 +1,31 @@
+//
+//  OTRoute.swift
+//  OpalTrip
+//
+//  Created by lazy on 2018/3/16.
+//  Copyright © 2018年 lazy. All rights reserved.
+//
+
+import UIKit
+
+public class OTRoute: NSObject {
+
+    // 定义三种切换控制器事件1.present 2.push 3.rootController
+    public class func changeRoot(clazzName: String, namespace: String ,parameters: [String: Any]? = nil) {
+        if let clazz = NSClassFromString("\(namespace).\(clazzName)") as? UIViewController.Type {
+            UIApplication.shared.keyWindow?.rootViewController = clazz.init()
+        }
+    }
+    
+    public class func present(clazzName: String, namespace: String, parameter: [String: Any]? = nil, atController: UIViewController) {
+        if let clazz = NSClassFromString("\(namespace).\(clazzName)") as? UIViewController.Type {
+            atController.present(clazz.init(), animated: true, completion: nil)
+        }
+    }
+    
+    public class func push(clazzName: String, namespace: String, parameter: [String: Any]? = nil, atController: UIViewController) {
+        if let clazz = NSClassFromString("\(namespace).\(clazzName)") as? UIViewController.Type {
+            atController.navigationController?.pushViewController(clazz.init(), animated: true)
+        }
+    }
+}
